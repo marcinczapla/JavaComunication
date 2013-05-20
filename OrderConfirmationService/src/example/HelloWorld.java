2bb61a59ac3e4b5e2cab15fb4d78808e26e4ff3a
Marcin Czapla <czapla.m@gmail.com>
2013-05-20 23:08:03 +0200

    webservice potwierdzania zamówień

---
+++ OrderConfirmationService/src/example/HelloWorld.java
@@ -0,0 +1,13 @@
+package example;/**
+ * Created with IntelliJ IDEA.
+ * User: Marcin
+ * Date: 20.05.13
+ * Time: 23:06
+ */
+public class HelloWorld {
+  public String sayHelloWorldFrom(String from) {
+    String result = "Hello, world, from " + from;
+    System.out.println(result);
+    return result;
+  }
+}
